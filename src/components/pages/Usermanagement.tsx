37ed6209fb28f978cd559aaae0ff1af6af498a8d
s1f102100793 <85666759+s1f102100793@users.noreply.github.com>
2023-04-09 00:27:33 +0900

    82 ログインユーザー情報をContextに保存してみる

--- src/components/pages/Usermanagement.tsx
+++ src/components/pages/Usermanagement.tsx
@@ -12,11 +12,14 @@ import { UserDetailModal } from "../organisms/user/UserDetailModal";
 import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
 import { useSelectUser } from "../../hooks/useSelectUser";
+import { useLoginUser } from "../../hooks/useLoginUser";
 
 export const UserManagement: VFC = memo(() => {
   const { onOpen, isOpen, onClose } = useDisclosure();
   const { getUsers, users, loading } = useAllUsers();
   const { onSelectUser, selectedUser } = useSelectUser();
+  const { loginUser } = useLoginUser();
+  console.log(loginUser);
 
   useEffect(() => getUsers, []);
 
